851821ff4cd3fbbdea4e21a5dc1872efb5b736e3
Olya Dubodel <dubodel.olya@gmail.com>
2024-12-14 22:04:57 +0300

    feat: solve +6 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -121,8 +121,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr.length) return 0;
+  const res = arr.reduce((sum, item) => sum + item, 0) / arr.length;
+  return Number(res.toFixed(2));
 }
 
 /**
@@ -135,8 +137,9 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const len = arr[0].length;
+  return arr.every((item) => item.length === len);
 }
 
 /**
@@ -150,8 +153,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((item, index) => item === index);
 }
 
 /**
